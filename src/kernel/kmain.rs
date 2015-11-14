754abf9d63b93bc3733767d3e9e9b08d2f48ff64
Stefan Nitz <nitz.stefan@googlemail.com>
2015-11-14 02:17:27 +0000

    added hang message

--- src/kernel/kmain.rs
+++ src/kernel/kmain.rs
@@ -13,7 +13,7 @@ fn print_mem(base: u64, length: u64, mem_type: multiboot::MemType) {
 
 #[lang="start"]
 #[no_mangle]
-extern "C" fn kmain(mboot_sig: u64, mboot_ptr: u64, pml4t_ptr: u64) {
+extern "C" fn kmain(mboot_sig: u64, mboot_ptr: u64, pml4t_ptr: u64) -> ! {
     log!("Hello World, this is kernel");
     log!("Hello {0}, this is kernel with println parameter \"{0}\"", "World");
     log!("kernel loaded at 0x{:X}", kernel_base);
@@ -34,9 +34,10 @@ extern "C" fn kmain(mboot_sig: u64, mboot_ptr: u64, pml4t_ptr: u64) {
     //    let pml4t = ::core::slice::from_raw_parts(pml4t_ptr as *const paging::PML4Entry, 512);
     //}
     
-    loop{}
+    hang();
 }
 
 fn hang() -> ! {
+    log!("system hangs now");
     loop {}
 }
